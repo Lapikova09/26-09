a41d3ec4cec9e09004be2c248b5e29e81e50f3db
Lapikova09 <96071466+Lapikova09@users.noreply.github.com>
2022-09-26 18:37:11 +0300

    Add files via upload

---
+++ Source.cpp Катерина2.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+int main() {
+	//int a;
+	//cin >> a;
+	//if (a > 0); {
+		//a += 1;// ƒано целое число.≈сли оно €вл€етс€ положительным, то прибавить к
+		//нему 1; в противном случае не измен€ть его.¬ывести полученное число.
+	//}
+	//cout << a << endl;
+	//system("pause");
+	//ƒано целое число.≈сли оно €вл€етс€ положительным, то прибавить к
+	//	нему 1; в противном случае вычесть из него 2. ¬ывести полученное число.	//int a;
+	//cin >> a;
+	//if (a > 0) {
+	//	a += 1; }
+	//else {
+	//	a -= 2;
+	//}
+	//cout << a << endl;
+	//ƒано целое число. ≈сли оно €вл€етс€ положительным, то прибавить к
+	//нему 1; если отрицательным, то вычесть из него 2; если нулевым, то
+		//заменить его на 10. ¬ывести полученное число.
+	//int a;
+	//cin >> a;
+	//if (a > 0) {
+	//	a += 1;
+	//}
+	//else if (a < 0) {
+	//	a -= 2;
+	//}
+	//else if (a == 0) {
+	//	a = 10;
+	//}
+	//cout << a << endl;
+	//system("pause");	//int a, b, c;
+	//int k = 0;
+	//cin >> a >> b >> c;
+	//if (a > 0) {
+	//	k++;
+	//}
+	//if (b > 0) {
+	//	k++;
+	//}
+	//if (c > 0) {
+	//	k++;
+	//}
+	//cout << k << endl;
+	//system("pause");
+	//int a, b, c;
+	//int k = 0;
+	//int n = 0;
+	//cin >> a >> b >> c;
+    //if (a > 0) {
+	//	k++;
+	//}
+	//if (b > 0) {
+	//	k++;
+	//}
+	//if (c > 0) {
+	//	k++;
+	//}
+	//if (a < 0) {
+	//	n++;
+	//}
+	//if (b < 0) {
+	//	n++;
+	//}
+	//if (c < 0) {
+	//	n++;
+	//}
+	//cout << k << endl;
+	//cout << n << endl;
+	//system("pause");
+	//int a, b;
+	//cin >> a >> b;
+	//if (a>b){ cout << a << endl; }
+	//else if (a<b) { cout << b << endl; }
+	//else if (a=b) { cout << "equals" << endl; }
+	//system("pause");
+	//int a, b;
+	//cin >> a >> b;
+	//if (a>b) { cout << "2" << endl; }
+	//else if (a<b) { cout << "1" << endl; }
+   //else if (a=b) { cout << "equals" << endl; }
+	//system("pause");
+	//int a, b;
+	//cin >> a >> b;
+	//if (a > b) { cout << a << endl; cout << b << endl; }
+	//else if (a < b) { cout << b << endl; cout << a << endl; }
+	//else if (a = b) { cout << "equals" << endl; }
+	//system("pause");
+	int a, b;
+	cin >> a >> b;
+	int c = 0;
+	if (a > b) { c = a; a = b; b = c; }
+	cout << a << endl; cout << b << endl;
+	system("pause");
+}
